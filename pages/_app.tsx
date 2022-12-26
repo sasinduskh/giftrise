0388a9ef93f7e9d3fd382db753d45334abf8d5c2
S K <110117611+sasinduskh@users.noreply.github.com>
2022-12-26 14:11:16 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
+    <>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RB8TGE2H4K"
+        ></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RB8TGE2H4K');`}
+        </script>
+      </Head>
+     <ThemeProvider attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
+    </>
+   
   );
 }
